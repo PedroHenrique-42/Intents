69de73aee3aa76b47b53c3a4fb079a62bbb84dcc
Pedro Henrique <phlferezin03@gmail.com>
2025-09-15 20:03:01 -0300

    feat: configurando listener no botão para abrir a ParameterActivity

--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -1,12 +1,18 @@
 package br.edu.scl.ifsp.sdm.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val PARAMETER_REQUEST_CODE = 0
+    }
+
     private val activityMainBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -17,8 +23,14 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
         supportActionBar?.subtitle = localClassName
 
-        activityMainBinding.parameterBt.setOnClickListener {
-
+        activityMainBinding.apply {
+            parameterBt.setOnClickListener {
+                val parameterIntent =
+                    Intent(this@MainActivity, ParameterActivity::class.java).apply {
+                        putExtra(PARAMETER_EXTRA, parameterTv.text)
+                    }
+                startActivityForResult(parameterIntent, PARAMETER_REQUEST_CODE)
+            }
         }
     }
 
@@ -32,6 +44,7 @@ class MainActivity : AppCompatActivity() {
             R.id.openActivityMi -> {
                 true
             }
+
             R.id.viewMi -> {
                 true
             }
